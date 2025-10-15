76581146a96becbc181c8c03054d7f5cbc8899d4
mouadh bali <bali.mouadh@gmail.com>
2025-10-15 13:57:52 +0100

    Update tp1.c

--- Algo/tp1.c
+++ Algo/tp1.c
@@ -13,6 +13,7 @@ int main() {
     printf("Enter the elements of the matrix:\n");
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
+			scanf("arr[%d][%d]",i,j);
             scanf("%d", &arr[i][j]);
         }
     }
